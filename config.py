a59410059c49c50b78f70c2dc25d71792323d697
Pigeon228 <149380745+Pigeon228@users.noreply.github.com>
2025-09-15 00:07:14 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
-# API-ключ OpenRouter (для демо решил тут на время оставить ключ с ограничением 3 доллара)
-OPENROUTER_API_KEY = "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"
+# API-ключ OpenRouter
+OPENROUTER_API_KEY = "sk-or-v1-"
 
 # Модель по умолчанию
 OPENROUTER_MODEL = "openai/gpt-5-mini"
